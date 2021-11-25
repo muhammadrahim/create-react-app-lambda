6c09726a690c91b3a7caf0c168ac878dc78d4884
Mo <rahim.muhammad@pm.me>
2021-11-25 18:39:23 +0100

    added html and css

--- src/App.js
+++ src/App.js
@@ -21,12 +21,34 @@ class LambdaDemo extends Component {
     const { loading, msg } = this.state
 
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+<body>
+  <div class="top">
+  </div>
+  <div class="form">
+    <div class="info">
+      <h1>You've been invited</h1>
+      <h2>for the wedding of</h2>
+      <h1>Ishmael & Sindy</h1>
+      <p class="line">________________________________________</p>
+      <h2>The Details</h2>
+      <p>Friday, December 3, 2021</p>
+      <p>4:00 PM</p>
+      <br>
+      <h2>Ceremony</h2>
+      <p>Amstelveen Town Hallr</p>
+      <p>Nieuwer-Amstel 1, 1182 JR Amstelveen</p>
+      <p class="line">________________________________________</p>
+      <div class='smallprint'>
+        I know some of you have mentioned gifts - my IBAN below if you really feel like you must give something.
+        <br>
+        Please don't feel forced to give something, anything is welcome as well as nothing at all!
+      </div>
+      <div class='bottom'>
+        NL32 ABNA 0861 4302 12
+      </div>
+    </div>
+  </div>
+</body>
     )
   }
 }
