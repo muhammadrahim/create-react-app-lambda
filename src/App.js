e46973dd77898c26436689723af922576523f5cf
Mo <rahim.muhammad@pm.me>
2021-11-25 18:39:54 +0100

    bug

--- src/App.js
+++ src/App.js
@@ -33,14 +33,14 @@ class LambdaDemo extends Component {
       <h2>The Details</h2>
       <p>Friday, December 3, 2021</p>
       <p>4:00 PM</p>
-      <br>
+      <br></br>
       <h2>Ceremony</h2>
       <p>Amstelveen Town Hallr</p>
       <p>Nieuwer-Amstel 1, 1182 JR Amstelveen</p>
       <p class="line">________________________________________</p>
       <div class='smallprint'>
         I know some of you have mentioned gifts - my IBAN below if you really feel like you must give something.
-        <br>
+        <br></br>
         Please don't feel forced to give something, anything is welcome as well as nothing at all!
       </div>
       <div class='bottom'>
