c4838ee998d8681c6fae8d9f9ea51127e9239cd8
Mo <rahim.muhammad@pm.me>
2021-11-26 07:13:53 +0100

    moving img

--- src/App.js
+++ src/App.js
@@ -20,34 +20,34 @@ class LambdaDemo extends Component {
 
     return (
 <body>
-  <div class="top">
-  </div>
-  <div class="form">
-    <div class="couple"></div>
-    <div class="info">
-      <h1>You've been invited</h1>
-      <h2>for the wedding of</h2>
-      <h1>Ishmael & Sindy</h1>
-      <p class="line">________________________________________</p>
-      <h2>The Details</h2>
-      <p>Friday, December 3, 2021</p>
-      <p>4:00 PM</p>
-      <br></br>
-      <h2>Ceremony</h2>
-      <p>Amstelveen Town Hall</p>
-      <p>Nieuwer-Amstel 1, 1182 JR Amstelveen</p>
-      <p class="line">________________________________________</p>
-      <div class='smallprint'>
-        I know some of you have mentioned gifts - my IBAN below if you really feel like you must give something.
+    <div class="top">
+    </div>
+    <div class="form">
+        <div class="info">
+        <img src="img.jpg">
+        <h1>You've been invited</h1>
+        <h2>for the wedding of</h2>
+        <h1>Ishmael & Sindy</h1>
+        <p class="line">________________________________________</p>
+        <h2>The Details</h2>
+        <p>Friday, December 3, 2021</p>
+        <p>4:00 PM</p>
         <br></br>
-        Please don't feel forced to give something, anything is welcome as well as nothing at all!
-      </div>
-      <div class='bottom'>
-        NL32 ABNA 0861 4302 12
+        <h2>Ceremony</h2>
+        <p>Amstelveen Town Hall</p>
+        <p>Nieuwer-Amstel 1, 1182 JR Amstelveen</p>
+        <p class="line">________________________________________</p>
+        <div class='smallprint'>
+          I know some of you have mentioned gifts - my IBAN below if you really feel like you must give something.
+          <br></br>
+          Please don't feel forced to give something, anything is welcome as well as nothing at all!
+        </div>
+        <div class='bottom'>
+          NL32 ABNA 0861 4302 12
+        </div>
       </div>
     </div>
-  </div>
-</body>
+  </body>
     )
   }
 }
