55295560c195b838672b88fb273ae5d44f367c2f
Mo <rahim.muhammad@pm.me>
2021-11-26 17:46:10 +0100

    change content

--- src/App.js
+++ src/App.js
@@ -26,21 +26,21 @@ class LambdaDemo extends Component {
         <div class="info">
         <img src="img.jpg" alt="Ishmael and Cindy"></img>
         <h1>You've been invited</h1>
-        <h2>for the wedding of</h2>
+        <h2>to the wedding of</h2>
         <h1>Ishmael & Sindy</h1>
-        <p class="line">________________________________________</p>
-        <h2>The Details</h2>
+        <p class="line">_____________________________________</p>
+        <h2>Date & Time</h2>
         <p>Friday, December 3, 2021</p>
         <p>4:00 PM</p>
         <br></br>
-        <h2>Ceremony</h2>
+        <h2>Location</h2>
         <p>Amstelveen Town Hall</p>
         <p>Nieuwer-Amstel 1, 1182 JR Amstelveen</p>
-        <p class="line">________________________________________</p>
+        <p class="line">_____________________________________</p>
         <div class='smallprint'>
           I know some of you have mentioned gifts - my IBAN below if you really feel like you must give something.
           <br></br>
-          Please don't feel forced to give something, anything is welcome as well as nothing at all!
+          Please don't feel forced to give something, anything is welcome - as well as nothing at all!
         </div>
         <div class='bottom'>
           NL32 ABNA 0861 4302 12
